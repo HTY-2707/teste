37fae3bb9f7c0fa44f4bf008e3e717f749c3ff9b
HTY-2707 <helio.yamada@yahoo.com.br>
2025-07-30 19:29:32 -0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const flashcards = [
+  {
+    question: "Qual a capital do Brasil?",
+    options: ["São Paulo", "Brasília", "Rio de Janeiro", "Salvador"],
+    answer: "Brasília",
+  },
+  {
+    question: "Quanto é 5 x 6?",
+    options: ["11", "30", "56", "25"],
+    answer: "30",
+  },
+  {
+    question: "Quem escreveu 'Dom Casmurro'?",
+    options: ["Machado de Assis", "Clarice Lispector", "Drummond", "Aluísio Azevedo"],
+    answer: "Machado de Assis",
+  },
+];
+
+export default flashcards;
